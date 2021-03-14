bbd468472b75377a900c2b6fda97fb894f69e232
Artsiom Murashko <ksarelto@tut.by>
2021-03-14 21:38:19 +0300

    feat:add a solution for task typical-array-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if (array === undefined || array.length === 0) {
+    return 0;
+  }
+
+  array.sort((a, b) => { return (a - b) })
+  return array[0];
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if (array === undefined || array.length === 0) {
+    return 0;
+  }
+
+  array.sort((a, b) => { return (a - b) })
+  return array[array.length - 1];
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if (array === undefined || array.length === 0) {
+    return 0;
+  }
+  var result = array.reduce((res, el) => { return (res += el) })
+  return (result / array.length);
 }
